80456f4b255d7a91bea89759912c86ff3fdc727a
Tim Lamborn <tlamborn@gmail.com>
2021-02-19 00:01:37 +1300

    WDG divider computed dynamically

--- Lib/WDG.c
+++ Lib/WDG.c
@@ -6,9 +6,11 @@
  * PRIVATE DEFINITIONS
  */
 
-// Note, if the prescalar is adjusted, so must the freq
-#define IWDG_PRESCALAR		IWDG_PRESCALER_128
-#define IWDG_FREQUENCY		(LSI_VALUE / 128)
+// Note: if the IWDG_PR_MIN is adjusted, so must the IWDG_FREQ_BASE
+#define IWDG_PR_MIN				IWDG_PRESCALER_4
+#define IWDG_PR_MAX				IWDG_PRESCALER_256
+#define IWDG_FREQ_BASE			(LSI_VALUE / 4)
+
 #define IWDG_RELOAD_MAX		0xFFF
 
 // Using div 128, this gives a max period of ~14s
@@ -21,6 +23,8 @@
 /*
  * PRIVATE PROTOTYPES
  */
+
+static uint32_t WDG_SelectPrescalar(uint32_t target_period, uint32_t * frequency);
 
 /*
  * PRIVATE VARIABLES
@@ -35,10 +39,11 @@ void WDG_Init(uint32_t period)
 	IWDG->KR = IWDG_KEY_ENABLE;
 	IWDG->KR = IWDG_KEY_WRITE_ACCESS_ENABLE;
 
-	IWDG->PR = IWDG_PRESCALAR;
+	uint32_t frequency;
+	IWDG->PR = WDG_SelectPrescalar(period, &frequency);
 
 	// +1 to prevent timer being shorter than expected.
-	uint32_t reload = (IWDG_FREQUENCY * period / 1000) + 1;
+	uint32_t reload = (frequency * period / 1000) + 1;
 	if (reload > IWDG_RELOAD_MAX) { reload = IWDG_RELOAD_MAX; }
 
 	IWDG->RLR = reload;
@@ -62,6 +67,19 @@ void WDG_Kick(void)
 /*
  * PRIVATE FUNCTIONS
  */
+
+static uint32_t WDG_SelectPrescalar(uint32_t target_period, uint32_t * frequency)
+{
+	uint32_t period_ticks = IWDG_FREQ_BASE * target_period / 1000;
+	uint32_t k = 0;
+	while (k <= IWDG_PR_MAX && period_ticks >= IWDG_RELOAD_MAX)
+	{
+		k++;
+		period_ticks >>= 1;
+	}
+	*frequency = IWDG_FREQ_BASE >> k;
+	return k << IWDG_PR_PR_Pos;
+}
 
 /*
  * INTERRUPT ROUTINES

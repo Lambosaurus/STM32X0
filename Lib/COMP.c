c46a077ed151b73f44a8bd48ac0c43cc933702e8
Tim Lamborn <tlamborn@gmail.com>
2021-06-12 23:36:05 +1200

    Changes for comparators

--- Lib/COMP.c
+++ Lib/COMP.c
@@ -5,6 +5,10 @@
  * PRIVATE DEFINITIONS
  */
 
+#ifndef COMP_IRQn
+#define COMP_IRQn		ADC1_COMP_IRQn
+#endif
+
 /*
  * PRIVATE TYPES
  */
@@ -17,30 +21,67 @@
  * PRIVATE VARIABLES
  */
 
+#ifdef COMP1_ENABLE
+static COMP_t gCOMP_1 = {
+	.Instance = COMP1,
+};
+COMP_t * COMP_1 = &gCOMP_1;
+#endif
+#ifdef COMP2_ENABLE
+static COMP_t gCOMP_2 = {
+	.Instance = COMP2,
+};
+COMP_t * COMP_2 = &gCOMP_2;
+#endif
+
 /*
  * PUBLIC FUNCTIONS
  */
 
-void COMP_Init(COMP_t * comp)
+void COMP_Init(COMP_t * comp, COMP_Input_t inputs)
 {
-
+	comp->Instance->CSR = inputs
+			| COMP_OUTPUTPOL_NONINVERTED
+			| COMP_POWERMODE_MEDIUMSPEED;
+	__HAL_COMP_ENABLE(comp);
 }
 
 void COMP_Deinit(COMP_t * comp)
 {
-
+#ifdef COMP_USE_IRQS
+	uint32_t exti = COMP_GET_EXTI_LINE(comp->Instance);
+	CLEAR_BIT(EXTI->IMR, exti);
+#endif
+	comp->Instance->CSR = 0;
 }
 
-void COMP_Start(COMP_t * comp)
+bool COMP_Read(COMP_t * comp)
 {
-
+	return hcomp->Instance->CSR & COMP_CSR_COMPxOUTVALUE;
 }
 
-void COMP_Stop(COMP_t * comp)
+#ifdef COMP_USE_IRQS
+void COMP_OnChange(COMP_t * comp, GPIO_IT_Dir_t dir, VoidFunction_t callback)
 {
+	comp->callback = callback;
 
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+    uint32_t exti = COMP_GET_EXTI_LINE(comp->Instance);
+	if(dir & GPIO_IT_Rising)
+	{
+		SET_BIT(EXTI->RTSR, exti);
+	}
+	if(dir & GPIO_IT_Falling)
+	{
+		SET_BIT(EXTI->FTSR, exti);
+	}
+	// Clear EXTI pending bit - in case its already set
+	WRITE_REG(EXTI->PR, exti);
+	SET_BIT(EXTI->IMR, exti);
+
+	HAL_NVIC_EnableIRQ(COMP_IRQn);
 }
-
+#ifdef COMP_USE_IRQS
 
 /*
  * PRIVATE FUNCTIONS
@@ -49,3 +90,26 @@ void COMP_Stop(COMP_t * comp)
 /*
  * INTERRUPT ROUTINES
  */
+
+#ifdef COMP_USE_IRQS
+static inline void COMP_IRQHandler(COMP_t * comp, uint32_t exti)
+{
+	if(EXTI->PR & exti)
+	{
+		WRITE_REG(EXTI->PR, exti);
+		comp->callback();
+	}
+}
+
+void ADC1_COMP_IRQHandler(void)
+{
+	// This will have to be shared with the ADC handler at some point...
+#ifdef COMP1_ENABLE
+	COMP_IRQHandler(COMP_1, COMP_EXTI_LINE_COMP1);
+#endif
+#ifdef COMP2_ENABLE
+	COMP_IRQHandler(COMP_2, COMP_EXTI_LINE_COMP2);
+#endif
+}
+#endif
+
--- Lib/COMP.h
+++ Lib/COMP.h
@@ -17,22 +17,48 @@
  * PUBLIC TYPES
  */
 
-typedef struct {
+// Note - some options based on the internal VREF exist, and could be supported in future.
+typedef enum {
+	COMP_Pos_IO1 = COMP_INPUT_PLUS_IO1,
+	COMP_Pos_IO2 = COMP_INPUT_PLUS_IO2,
+	COMP_Pos_IO3 = COMP_INPUT_PLUS_IO3,
+	COMP_Pos_IO4 = COMP_INPUT_PLUS_IO4,
+	COMP_Pos_IO5 = COMP_INPUT_PLUS_IO5,
+#ifdef COMP_INPUT_PLUS_IO6
+	COMP_Pos_IO6 = COMP_INPUT_PLUS_IO6,
+#endif
+	COMP_Neg_IO1 = COMP_INPUT_MINUS_IO1,
+	COMP_Neg_IO2 = COMP_INPUT_MINUS_IO2,
+} COMP_Input_t;
 
+typedef struct {
+	COMP_TypeDef Instance;
+#ifdef COMP_USE_IRQS
+	VoidFunction_t callback;
+#endif
 }COMP_t;
 
 /*
  * PUBLIC FUNCTIONS
  */
 
-void COMP_Init(COMP_t * comp);
+void COMP_Init(COMP_t * comp, COMP_Input_t inputs);
 void COMP_Deinit(COMP_t * comp);
+bool COMP_Read(COMP_t * comp);
 
-void COMP_Start(COMP_t * comp);
-void COMP_Stop(COMP_t * comp);
+#ifdef COMP_USE_IRQS
+void COMP_OnChange(COMP_t * comp, GPIO_IT_Dir_t dir, VoidFunction_t callback);
+#endif
 
 /*
  * EXTERN DECLARATIONS
  */
+
+#ifdef COMP1_ENABLE
+extern COMP_t * COMP_1;
+#endif
+#ifdef COMP2_ENABLE
+extern COMP_t * COMP_2;
+#endif
 
 #endif //COMP_H
